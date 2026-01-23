ec0586aea404c506d676589d85e0be43a82914a2
Aishani Dagar <aishanid@andrew.cmu.edu>
2026-01-23 13:01:29 +0000

    getRecentPosts-Not working

--- src/posts/recent.js
+++ src/posts/recent.js
@@ -5,15 +5,16 @@ const _ = require('lodash');
 const db = require('../database');
 const privileges = require('../privileges');
 
-
+console.log('Aishani Dagar');
 module.exports = function (Posts) {
 	const terms = {
 		day: 86400000,
 		week: 604800000,
 		month: 2592000000,
 	};
-
-	Posts.getRecentPosts = async function (uid, start, stop, term) {
+	console.log('Aishani Dagar');
+	Posts.getRecentPosts = async function (uid, {start, stop, term}) {
+		console.log('Aishani Dagar');
 		let min = 0;
 		if (terms[term]) {
 			min = Date.now() - terms[term];
@@ -24,10 +25,11 @@ module.exports = function (Posts) {
 		pids = await privileges.posts.filter('topics:read', pids, uid);
 		return await Posts.getPostSummaryByPids(pids, uid, { stripTags: true });
 	};
-
+	console.log('Aishani Dagar');
 	Posts.getRecentPosterUids = async function (start, stop) {
 		const pids = await db.getSortedSetRevRange('posts:pid', start, stop);
 		const postData = await Posts.getPostsFields(pids, ['uid']);
 		return _.uniq(postData.map(p => p && p.uid).filter(uid => parseInt(uid, 10)));
 	};
 };
+console.log('Aishani Dagar');
